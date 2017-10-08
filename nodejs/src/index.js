db557b903eb2befe638455d4a180c9811f83cf1d
Florian Schinagl <fs@think-clean.de>
2017-10-08 17:52:22 +0200

    add docker-compose.yml

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,9 +1,5 @@
-const gracefulExit = require('express-graceful-exit');
-
 const app = require('./server');
 const db = require('./db');
-
-app.use(gracefulExit.middleware(app));
 
 const server = app.listen(3000, () => {
   console.log('Now listening on port 3000');
@@ -14,12 +10,4 @@ const server = app.listen(3000, () => {
       console.error(err.toString());
       server.close();
     });
-});
-
-process.on('message', (message) => {
-  if (message === 'shutdown') {
-    gracefulExit.gracefulExitHandler(app, server, {
-      socketio: app.settings.socketio,
-    });
-  }
 });
